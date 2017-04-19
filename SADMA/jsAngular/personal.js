ffa6b0f3f8727dace4c4b5206da9c118b9d51926
francisco <sonnik42@hotmail.com>
2017-04-19 00:37:36 -0300

    Avances

--- SADMA/jsAngular/personal.js
+++ SADMA/jsAngular/personal.js
@@ -15,4 +15,16 @@ app.controller("angularControl", function ($scope, $http) {
 									function (status) {
 									$scope.personalList = status;
 									});
+		var resLibroVuelo = $http.get("http://localhost:8080/libroVuelo");
+												 resLibroVuelo.success(
+																function (status) {
+																$scope.libroHoras = status;
+																$('#tabLibroHoras').find('div.form-group.form-md-line-input.form-md-floating-label.has-success').removeClass('form-md-floating-label');
+															});
+		var resLibroHorasList = $http.get("http://localhost:8080/libroHoraList");
+																									 resLibroHorasList.success(
+																													function (status) {
+																													$scope.libroHorasList = status;
+																													$('#tabLibroHoras').find('div.form-group.form-md-line-input.form-md-floating-label.has-success').removeClass('form-md-floating-label');
+																												});
 });
